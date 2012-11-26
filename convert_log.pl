5b979dc770f46684e720965b7eb89aeaac72da04
Anthony Willard <awillard69@yahoo.com>
2012-11-26 15:44:44 -0600

    Initial commit
    Initial commit

---
+++ convert_log.pl
@@ -0,0 +1,169 @@
+#!/usr/bin/perl -w
+
+# Convert a Cabrillo formatted log file from N1MM, as an ARCI contest, to 
+# a NAQCC sprint submission log data.
+#
+# Anthony Willard
+# 2012-11-26
+#
+use strict;
+# set syntax warnings to errors, avoiding things like '=' versus '=='
+use warnings FATAL => qw( syntax );
+use Getopt::Long;
+
+# setup
+$/="\n"; # default input record delimiter
+$,="\t"; # default output field delimiter
+$\="\n"; # default output record delimiter
+
+my( $inputfile, $outputfile, $helpfile );
+
+GetOptions( "h" => \$helpfile,
+		"i|input=s" => \$inputfile,
+        "o|output=s" => \$outputfile
+        );
+
+# show any help if requested...
+usage() if( defined( $helpfile ) );
+
+# if we don't have any input log file, we have to stop...
+if( !defined( $inputfile ) )
+{
+	print STDERR "Unable to proceed without a source input file.";
+	print STDERR "";
+	
+	usage();
+}
+
+# open the input file...
+open( INPUT, "<", $inputfile ) || die( "Cannot open input file $inputfile: \"$!\"" );
+
+# if we have a designated output file, use it
+if( defined( $outputfile ) )
+{
+	open( OUTPUT, ">", $outputfile ) || die( "Cannot open input file $outputfile: \"$!\"" );
+}
+else
+{
+	# otherwise, send the output to STDERR
+	*OUTPUT = *STDERR;
+}
+
+my ( $qsocount, $membercount, $spccount, %spcmults, $ismember, $formatspec  );
+
+# defaults
+$qsocount = 0;
+$membercount = 0;
+$spccount = 0;
+
+# set the format output spec
+$formatspec = "%-11.11s%-4.4s%-5.5s%-11.11s%-4.4s%-7.7s%-8.8s%-4.4s";
+
+# output a file header
+print OUTPUT sprintf( $formatspec, "Call", "Bnd", "Time", "Worked", "SPC", "Nr/Pwr", "NewMult", "Pts" );
+
+# QSO fields of interest
+my ( $call, $band, $time, $worked, $spc, $power );
+
+# read the input file...
+while( <INPUT> )
+{
+	chomp();
+	my $line = $_;
+	
+	# only process the QSO lines
+	if( $line =~ /^QSO/ )
+	{
+		# increment the QSO count
+		$qsocount++;
+		
+		# pluck out the fields
+		$time = substr( $line, 25, 4 );
+		$call = substr( $line, 30, 13 );
+		$worked = substr( $line, 58, 13 );
+		$spc = substr( $line, 77, 4 );
+		$power = substr( $line, 82, 7 );
+		$band = map_band( substr( $line, 4, 6 ) );
+		
+		# prepare any formatting by stripping any unwanted characters, like spaces
+		$call =~ s/[ ]*//g;
+		$worked =~ s/[ ]*//g;
+		$spc =~ s/[ ]*//g;
+		$power =~ s/[ ]*//g;
+		
+		# set some counter and tracking values...
+		$spcmults{ $spc } += 1;
+		$spccount = keys( %spcmults );
+		
+		# basic point for any QSO
+		$ismember = 1;
+		
+		# set the member rate
+		$ismember = 2 if( $power =~ /^[0-9][0-9][0-9][0-9]$/ );
+		
+		# increment the member counter
+		$membercount++ if( $ismember == 2 );
+
+		# if not a member, verify the power value is properly indicated
+		# at this point we only warn...
+		if( $ismember == 1 ) # nonmember, should have a power value
+		{
+			# if it doesn't look like #W, notify
+			if( !( $power =~ /[0-9]+[M]*W/ ) )
+			{
+				print STDERR "***QSO with nonmember $worked at $time shows incorrect power";
+			}
+		}
+		
+		# members are members forever, but what if the number looks good but is invalid?
+		# how do we compare it for accuracy, maybe look at the naqcc.dat file?  perhaps, check
+		# the call with the number and exclude mismatched data?
+		
+		print OUTPUT sprintf( $formatspec, $call, $band, $time, $worked, $spc, $power, ($spcmults{ $spc } == 1 ? $spccount : "-"), $ismember );
+	}
+}
+
+# housekeeping...
+close( INPUT );
+
+# summary time...
+print STDERR "";
+print STDERR "Summary stats for file $inputfile";
+print STDERR "";
+print STDERR "QSO Count    : " . $qsocount;
+print STDERR "Member count : " . $membercount;
+print STDERR "SPC Count    : " . $spccount;
+
+# put commas into the final score summary
+my $score = ( ( $qsocount + $membercount ) * $spccount );
+$score =~ s/(\d)(?=(\d{3})+(\D|$))/$1\,/g;
+
+print STDERR "Total points : " . $score;
+
+exit;
+
+# map a frequency to a band designator
+sub map_band
+{
+	my ($freq) = @_;
+	
+	$freq =~ s/[ ]*//g;
+	return( "80" ) if ( $freq =~ /^3[0-9][0-9][0-9]$/ );
+	return( "40" ) if ( $freq =~ /^7[0-9][0-9][0-9]$/ );
+	return( "20" ) if ( $freq =~ /^14[0-9][0-9][0-9]$/ );
+	
+	print STDERR "***Invalid band designation for QSO with $worked at $time z";
+	return( "99" );
+}
+
+# usage
+sub usage
+{
+	print STDERR "convert_log.pl: convert a Cabrillo format log from N1MM into the format required for a NAQCC Sprint format.";
+	print STDERR "Usage";
+	print STDERR "convert_log.pl -[i|input]=<log file> [-[o|output]=<output file>]";
+	print STDERR "\t<log file> is the name of the Cabrillo log file from N1MM";
+	print STDERR "\t<output file> is the file to create with the reformatted data, if not provided, output goes to the screen";
+	
+	exit( 99 );
+}
